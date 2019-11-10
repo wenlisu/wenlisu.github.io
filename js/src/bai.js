bc4ca5ca778eb2063b7b6e6672ccf67466477202
lisuWen <415357008@qq.com>
2019-11-10 20:46:29 +0800

    Site updated: 2019-11-10 20:46:28

---
+++ js/src/bai.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    var bp = document.createElement('script');
+    bp.src = '//push.zhanzhang.baidu.com/push.js';
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+});
